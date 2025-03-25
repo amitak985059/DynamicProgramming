33cfce5aed39b1d88c2ae5e32073daad1c31b4a8
Amit <amitak985059@gmail.com>
2025-03-25 21:15:05 +0530

    n

---
+++ Dp/Maximum sum of non adjacent elements/maxSumOfNonAdjeacent.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+int f(int ind, vector<int> &nums, vector<int> &dp)
+{
+    if (ind == 0)
+        return nums[ind];
+    if (ind < 0)
+        return 0;
+    if (dp[ind] != -1)
+        return dp[ind];
+
+    int pick = nums[ind] + f(ind - 2, nums);
+    int notPick = 0 + f(ind - 1, nums);
+
+    return dp[ind] = (pick, notPick);
+}
+int maximumNonAdjacentSum(vector<int> &nums)
+{
+    // Write your code here.
+    int n = nums.size();
+    vector<int> dp(n, -1);
+    return f(n - 1, nums, dp);
+}
---
+++ Dp/frogJumpWithKDistances/frogJumpWithKDistances.cpp
@@ -0,0 +1,4 @@
+int minimizeCost(int n, int k, vector<int> &height){
+    // Write your code here.
+    solve()
+}
