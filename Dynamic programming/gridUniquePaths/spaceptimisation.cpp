864141a5fb85cc594993927c4a3b4aadbe93e2eb
Amit <amitak985059@gmail.com>
2025-03-28 18:28:24 +0530

    comp

---
+++ Dynamic programming/gridUniquePaths/spaceptimisation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int uniquePaths(int m, int n) {
+            vector<int> prev(n, 1);
+    
+            for(int i = 1; i < m; i++) {
+                vector<int> cur(n, 1);
+                for(int j = 1; j < n; j++) {
+                    cur[j] = cur[j - 1] + prev[j];
+                }
+                prev = cur;
+            }
+            return prev[n - 1];
+        }
+    };
+    
